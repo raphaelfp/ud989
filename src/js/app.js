db0172108866096bd6c503b814c1bc058ee928ed
Raphael Fernandes <raphaelfp.eng@gmail.com>
2017-07-15 18:13:06 -0300

    Cat Clicker Requirements 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,22 @@
 require('../css/main.css');
 
-let counter = 0;
-let catClickerCounterElement = document.getElementById('cat-clicker-counter');
-let catClickerElement = document.getElementById('cat-clicker');
+let counter1 = 0;
+let catClickerCounterElement1 = document.getElementById('cat-clicker1-counter');
+let catClickerElement1= document.getElementById('cat-clicker1');
 
-catClickerCounterElement.innerHTML = counter;
-catClickerElement.addEventListener('click', () => {
-    counter++;
-    catClickerCounterElement.innerHTML = counter;
+catClickerCounterElement1.innerHTML = counter1;
+catClickerElement1.addEventListener('click', () => {
+    counter1++;
+    catClickerCounterElement1.innerHTML = counter1;
+}, false);
+
+
+let counter2 = 0;
+let catClickerCounterElement2 = document.getElementById('cat-clicker2-counter');
+let catClickerElement2 = document.getElementById('cat-clicker2');
+
+catClickerCounterElement2.innerHTML = counter2;
+catClickerElement2.addEventListener('click', () => {
+    counter2++;
+    catClickerCounterElement2.innerHTML = counter2;
 }, false);
