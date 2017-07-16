cb02c86c78f3c62e945dee9bc1159dfd4981e595
Raphael Fernandes <raphaelfp.eng@gmail.com>
2017-07-16 01:10:21 -0300

    Cat Clicker Premium refactoring

--- cat-clicker-premium/src/js/app.js
+++ cat-clicker-premium/src/js/app.js
@@ -12,43 +12,96 @@ class Cat {
     }
 }
 
-$( () => {
-    const getNewCatId = (() => {
+const helper = {
+    getNewCatId: (() => {
         let counter = 0;
         return () => {return counter++;}
-    })();
+    })(),
+};
 
-    let $catList = $('#cat-list');
-    let $displayArea = $('#display-area');
+const model = {
+    currentCat: null,
 
-    let cats = new Set([
-        new Cat('https://static.pexels.com/photos/60224/pexels-photo-60224.jpeg', 'Harold', getNewCatId()),
-        new Cat('https://static.pexels.com/photos/20787/pexels-photo.jpg', 'Mr. Potts', getNewCatId()),
-        new Cat('https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg', 'Kitty', getNewCatId()),
-        new Cat('https://upload.wikimedia.org/wikipedia/commons/2/28/Tortoiseshell_she-cat.JPG', 'Sheen', getNewCatId()),
-        new Cat('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Savannah_Cat_closeup.jpg/1280px-Savannah_Cat_closeup.jpg', 'Bob', getNewCatId())
-    ]);
+    cats: new Array(
+        new Cat('https://static.pexels.com/photos/60224/pexels-photo-60224.jpeg', 'Harold', helper.getNewCatId()),
+        new Cat('https://static.pexels.com/photos/20787/pexels-photo.jpg', 'Mr. Potts', helper.getNewCatId()),
+        new Cat('https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg', 'Kitty', helper.getNewCatId()),
+        new Cat('https://upload.wikimedia.org/wikipedia/commons/2/28/Tortoiseshell_she-cat.JPG', 'Sheen', helper.getNewCatId()),
+        new Cat('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Savannah_Cat_closeup.jpg/1280px-Savannah_Cat_closeup.jpg', 'Bob', helper.getNewCatId())
+    )
+};
 
-    for(let cat of cats) {
-        $catList.append(`<li><a id="cat-${cat.id}" href="#">${cat.name}</a></li>`);
-        $(`#cat-${cat.id}`).on('click', () => {
-            $displayArea.empty();
-            $displayArea.append(
-                `<div class="col-xs-12 col-md-offset-3 col-md-6">
-                    <h2>${cat.name}</h2>
-                    <div>
-                        <img class="img-responsive" id="cat-clicker${cat.id}" src="${cat.src}" alt="Say hi to ${cat.name}">
-                    </div>
-                    <p>
-                        You just clicked ${cat.name} <span id="cat-clicker${cat.id}-counter">${cat.counter}</span> times
-                    </p>
-                </div>`
-            );
-            $(`#cat-clicker${cat.id}`).on('click', () => {
-                cat.incrementCounter();
-                $(`#cat-clicker${cat.id}-counter`).html(cat.counter);
-            });
-        });
+const octopus = {
+    init: function() {
+        model.currentCat = model.cats[0];
+        catListView.init();
+        catView.init();
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
+
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
+
+    incrementCounter: function() {
+        model.currentCat.counter++;
+        catView.render();
     }
+};
 
-});
+const catView = {
+    init: function() {
+        this.$catName = $('#cat-name');
+        this.$catClicker = $('#cat-clicker');
+        this.$catCounter = $('#cat-counter');
+
+        this.$catClicker.on('click', () => {
+            octopus.incrementCounter();
+        });
+
+        this.render();
+    },
+
+    render: function() {
+        let cat = octopus.getCurrentCat();
+        this.$catName.html(cat.name);
+        this.$catClicker.attr('src', cat.src);
+        this.$catCounter.html(`You have clicked <span class="badge">${cat.counter}</span> times on ${cat.name}!!!`);
+    }
+};
+
+const catListView = {
+    init: function() {
+        this.render();
+    },
+
+    render: function() {
+        let cats = octopus.getCats();
+        const $catList = $('#cat-list');
+
+        $catList.empty();
+
+        for(const cat of cats) {
+            const $listElement = $('<li>').append($('<a>').html(cat.name).attr('href', '#'));
+            $listElement.find('a').on('click', function() {
+                octopus.setCurrentCat(cat);
+                for(const child of $catList.find('.active')) {
+                    console.log($(child).removeClass('active'));
+                }
+                $listElement.addClass('active');
+                catView.render();
+            });
+            $catList.append($listElement);
+            $catList.find(':first-child').addClass('active');
+        }
+    }
+};
+
+octopus.init();
